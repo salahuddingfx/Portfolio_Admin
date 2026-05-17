8209e6fa2a12783789315b8d6b97a046c6c9464f
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-17 23:40:18 +0600

    feat: implement reviews management page for viewing, filtering, and inviting testimonials

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -355,7 +355,7 @@ export default function ReviewsPage() {
                   </div>
                 </div>
               </div>
-            ))}
+            )))}
           </div>
         )}
       </div>
